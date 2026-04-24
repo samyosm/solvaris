82d0e283bcaa9d5be189a1a28ca3460cb881f4e2
samyosm <samy.osmium@gmail.com>
2026-04-24 01:33:31 -0400

    added example

--- solvaris/wrappers.py
+++ solvaris/wrappers.py
@@ -3,13 +3,7 @@ from . import solvaris
 
 def quadratic(a, b, c):
     x1, x2, status = solvaris.quadratic(a, b, c)
-    if status == 0 or status == -2:
-        raise "Error"
-    elif status == 1:
-        return x1
-    elif status == 2:
-        return x1, x2
-    raise "Unkown status"
+    return x1, x2
 
 
 def add(a, b):
