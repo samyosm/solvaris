987604c3bbf0fad1ceb4fdf5966064f6cdd1abd5
samyosm <samy.osmium@gmail.com>
2026-04-24 12:11:49 -0400

    fixed CMake

--- solvaris/__init__.py
+++ solvaris/__init__.py
@@ -1,1 +1,14 @@
-from .wrappers import *
+from . import _solvaris
+
+
+def quadratic(a, b, c):
+    x1, x2, status = _solvaris.quadratic(a, b, c)
+    return x1, x2
+
+
+def add(a, b):
+    return _solvaris.add(a, b)
+
+
+def sub(a, b):
+    return _solvaris.sub(a, b)
